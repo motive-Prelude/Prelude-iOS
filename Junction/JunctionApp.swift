a2575e3de9bf469012c92cfa73da99daf7f4df52
Eric <real.dassy@gmail.com>
2024-08-10 08:47:15 +0900

    first commit

---
+++ Junction/JunctionApp.swift
@@ -0,0 +1,17 @@
+//
+//  JunctionApp.swift
+//  Junction
+//
+//  Created by 송지혁 on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct JunctionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
