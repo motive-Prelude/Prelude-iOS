c327860fe9c6b6a8f69e22d3c26ee8a9d0bee8ec
Eric <real.dassy@gmail.com>
2024-08-10 09:21:07 +0900

    feat: MessageRepository 프로토콜 구현
    프로토콜 구현체에서 구현될 메서드들 정의

---
+++ Junction/Domain/Interfaces/Repositories/MessageRepository.swift
@@ -0,0 +1,14 @@
+//
+//  MessageRepository.swift
+//  bottari
+//
+//  Created by 송지혁 on 7/22/24.
+//
+
+import Combine
+
+
+protocol MessageRepository {
+    func createMessage(threadID: String, content: String) -> AnyPublisher<CreateMessageResponse, Error>
+    func retrieveMessage(threadID: String, messageID: String) -> AnyPublisher<RetrieveMessageResponse, Error>
+}
