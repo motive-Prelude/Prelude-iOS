ece2c56b26e4091b29a64b0760b804cdcdcd3f86
Eric <real.dassy@gmail.com>
2024-08-10 09:21:20 +0900

    feat: MessageRepositoryImpl 구현

---
+++ Junction/Data/Repositories/MessageRepositoryImpl.swift
@@ -0,0 +1,32 @@
+//
+//  MessageRepositoryImpl.swift
+//  bottari
+//
+//  Created by 송지혁 on 7/22/24.
+//
+
+import Combine
+import Foundation
+
+final class MessageRepositoryImpl: MessageRepository {
+    private let apiService: APIService
+    
+    init(apiService: APIService) {
+        self.apiService = apiService
+    }
+    
+    func createMessage(threadID: String, content: String) -> AnyPublisher<CreateMessageResponse, Error> {
+        let body = apiService.createMessageBody(role: "user", content: content)
+        
+        guard let request = apiService.createRequest(withURL: EndPoint.messages(threadID: threadID).urlString, body: body as MessageContent) else { return Fail(error: URLError(.badURL)).eraseToAnyPublisher() }
+        
+        return apiService.fetchData(with: request)
+    }
+    
+    
+    func retrieveMessage(threadID: String, messageID: String) -> AnyPublisher<RetrieveMessageResponse, any Error> {
+        guard let request = apiService.createRequest(withURL: EndPoint.retrieveMessage(threadID: threadID, messageID: messageID).urlString, method: "GET", body: nil as String?) else { return Fail(error: URLError(.badURL)).eraseToAnyPublisher() }
+        
+        return apiService.fetchData(with: request)
+    }
+}
