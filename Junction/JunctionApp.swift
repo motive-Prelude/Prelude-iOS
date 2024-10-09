a25d7d53027fe77f9568b1ce32c1b6c4451999a8
Eric <real.dassy@gmail.com>
2024-10-09 11:41:27 +0900

    feat: Store 클래스 EnvironmentObject 설정
    앱 전역에서 같은 Store 인스턴스에 접근할 수 있도록 environmentObject로 설정했습니다.

--- Junction/JunctionApp.swift
+++ Junction/JunctionApp.swift
@@ -9,10 +9,13 @@ import SwiftUI
 
 @main
 struct JunctionApp: App {
+    @StateObject var store = Store()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(NavigationManager())
+                .environmentObject(store)
         }
     }
 }
