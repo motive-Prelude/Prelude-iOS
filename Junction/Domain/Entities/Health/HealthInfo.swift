34cc7873ff3eff6f9ef7ade8ea7017e535490978
Eric <real.dassy@gmail.com>
2024-10-14 09:02:54 +0900

    feat: HealthInfo 모델 class로 변경
    SwiftData 적용을 위해 HealthInfo 모델을 class로 변경했습니다.

--- Junction/Domain/Entities/Health/HealthInfo.swift
+++ Junction/Domain/Entities/Health/HealthInfo.swift
@@ -5,14 +5,32 @@
 //  Created by 송지혁 on 8/11/24.
 //
 
-struct HealthInfo: Equatable, Hashable, Codable {
-    var pregnantWeek: PregnantWeek
-    var height: String
-    var weight: String
-    var bloodPressure: BloodPresure
-    var diabetes: Diabetes
+import CloudKit
+import SwiftData
+
+@Model
+final class HealthInfo {
+    var id: String
+    var pregnantWeek: PregnantWeek = PregnantWeek.early
+    var height: String = ""
+    var weight: String = ""
+    var bloodPressure: BloodPresure = BloodPresure.normal
+    var diabetes: Diabetes = Diabetes.notAffected
     
     var bmi: Double {
-        return Double(weight)! / Double(height)! * Double(height)!
+        guard let weightValue = Double(weight), let heightValue = Double(height) else { return 0.0 }
+        return weightValue / heightValue * heightValue
+    }
+    
+    init(id: String = "HealthInfo", pregnantWeek: PregnantWeek, height: String, weight: String, bloodPressure: BloodPresure, diabetes: Diabetes) {
+        self.id = id
+        self.pregnantWeek = pregnantWeek
+        self.height = height
+        self.weight = weight
+        self.bloodPressure = bloodPressure
+        self.diabetes = diabetes
+    }
+}
+
     }
 }
