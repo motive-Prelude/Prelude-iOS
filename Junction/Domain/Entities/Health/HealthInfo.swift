c242428d3d0b82ee940efce0a5c0c145e9275e61
Eric <real.dassy@gmail.com>
2024-10-14 09:08:32 +0900

    feat: HealthInfo 모델 Convertible 프로토콜 채택 및 구현
    HealthInfo 모델에 Convertible 프로토콜을 채택하고 관련 코드를 구현했습니다.

--- Junction/Domain/Entities/Health/HealthInfo.swift
+++ Junction/Domain/Entities/Health/HealthInfo.swift
@@ -32,5 +32,32 @@ final class HealthInfo {
     }
 }
 
+extension HealthInfo: Convertible {
+    func toCKRecord() -> CKRecord {
+        let recordID = CKRecord.ID(recordName: self.id)
+        let record = CKRecord(recordType: "HealthInfo", recordID: recordID)
+        
+        record["pregnantWeek"] = self.pregnantWeek.rawValue as CKRecordValue
+        record["height"] = self.height as CKRecordValue
+        record["weight"] = self.weight as CKRecordValue
+        record["bloodPressure"] = self.bloodPressure.rawValue as CKRecordValue
+        record["diabetes"] = self.diabetes.rawValue as CKRecordValue
+        
+        return record
+    }
+    
+    convenience init?(from record: CKRecord) {
+        let id = record.recordID.recordName
+        guard let pregnantWeekRaw = record["pregnantWeek"] as? String,
+              let pregnantWeek = PregnantWeek(rawValue: pregnantWeekRaw),
+              let height = record["height"] as? String,
+              let weight = record["weight"] as? String,
+              let bloodPressureRaw = record["bloodPressure"] as? String,
+              let bloodPressure = BloodPresure(rawValue: bloodPressureRaw),
+              let diabetesRaw = record["diabetes"] as? String,
+              let diabetes = Diabetes(rawValue: diabetesRaw)
+        else { return nil }
+        
+        self.init(id: id, pregnantWeek: pregnantWeek, height: height, weight: weight, bloodPressure: bloodPressure, diabetes: diabetes)
     }
 }
