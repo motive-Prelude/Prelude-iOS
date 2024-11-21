ce552153ec305634f66db59c7fba18a10df0a97a
Eric <real.dassy@gmail.com>
2024-11-21 10:01:25 +0900

    [CHORE] ImageClassifierUseCase 출력 확인용 임시 코드 작성

--- Junction/Presentation/ViewModels/ResultViewModel.swift
+++ Junction/Presentation/ViewModels/ResultViewModel.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 class ResultViewModel: ObservableObject {
     private let assistantInteractionFacade: AssistantInteractionFacadeImpl
+    private let performOCRUseCase: PerformOCRUseCase
     private let predictFoodUseCase: PredictFoodUseCase
+    private let predictFoodTextUseCase: PredictFoodTextUseCase
+    private let imageClassifierUseCase: ImageClassifierUseCase
+    
     private let cloudkitManager: CloudKitManager
     private let userStore: UserStore
     
@@ -28,28 +32,29 @@ class ResultViewModel: ObservableObject {
         retrieveMessageUseCase: DIContainer.shared.resolve(RetrieveMessageUseCase.self)!,
         uploadImageUseCase: DIContainer.shared.resolve(UploadImageUseCase.self)!
     ),
+         performOCRUseCase: PerformOCRUseCase = DIContainer.shared.resolve(PerformOCRUseCase.self)!,
          predictFoodUseCase: PredictFoodUseCase = DIContainer.shared.resolve(PredictFoodUseCase.self)!,
+         predictFoodTextUseCase: PredictFoodTextUseCase = DIContainer.shared.resolve(PredictFoodTextUseCase.self)!,
+         imageClassifierUseCase: ImageClassifierUseCase = DIContainer.shared.resolve(ImageClassifierUseCase.self)!,
          cloudKitManager: CloudKitManager = CloudKitManager.shared,
          userStore: UserStore = .shared
     ) {
         
         self.assistantInteractionFacade = assistantInteractionFacade
+        self.performOCRUseCase = performOCRUseCase
         self.predictFoodUseCase = predictFoodUseCase
         self.cloudkitManager = cloudKitManager
         self.userStore = userStore
+        self.predictFoodTextUseCase = predictFoodTextUseCase
+        self.imageClassifierUseCase = imageClassifierUseCase
     }
     
     
     func checkRemainingTimes() async -> Bool {
         await self.userStore.fetchUserInfo()
-        guard let userInfo = userStore.userInfo else {
-            print("없나..")
-            return false
-        }
-        print("있나")
+        guard let userInfo = userStore.userInfo else { return false }
         
         //        if newUserInfo.remainingTimes < 1 {
-        //            print("작음")
         //            return false
         //        }
         
@@ -61,26 +66,64 @@ class ResultViewModel: ObservableObject {
         return true
     }
     
-    func detectFoodOrNot(image: UIImage, completion: @escaping (Bool) -> Void) {
-        predictFoodUseCase.execute(with: image) { result in
+    private func extractText(in image: UIImage, completion: @escaping ([String]) -> Void) {
+        performOCRUseCase.execute(image: image) { result in
             switch result {
-                case .success(let foodType):
-                    print(foodType)
-                    if foodType == "Nonfood" {
-                        print("음식 아님")
-                        completion(false)
-                        return
-                        
-                    } else {
-                        print("음식임")
-                        completion(true)
-                        return
-                    }
-                case .failure:
-                    completion(false)
+                case .success(let text):
+                    completion(text)
+                    return
+                case .failure(let error):
+                    print(error)
                     return
             }
         }
+    }
+    
+    func detectFoodOrNot(image: UIImage, completion: @escaping (Bool) -> Void) {
+        guard let result = imageClassifierUseCase.classify(image: image) else {
+            print("문제 생김")
+            completion(false)
+            return
+        }
+        if result == "a Nonfood" {
+            print("음식 아니다")
+            completion(false)
+        }
+        else {
+            print("음식이다")
+            completion(true)
+        }
+        
+//        predictFoodUseCase.execute(with: image) { result in
+//            switch result {
+//                case .success(let foodType):
+//                    print(foodType)
+//                    if foodType == "Nonfood" {
+//                        self.extractText(in: image) { texts in
+//                            if self.detectFoodTextOrNot(texts: texts) {
+//                                print("음식 텍스트 인식")
+//                                completion(true)
+//                            }
+//                            else {
+//                                print("음식이 아닌 텍스트")
+//                                completion(false)
+//                            }
+//                        }
+//                        return
+//                        
+//                    } else {
+//                        completion(true)
+//                        return
+//                    }
+//                case .failure:
+//                    completion(false)
+//                    return
+//            }
+//        }
+    }
+    
+    private func detectFoodTextOrNot(texts: [String]) -> Bool {
+        predictFoodTextUseCase.execute(texts: texts)
     }
     
     func sendMessage(_ message: String, image: UIImage?) async -> Bool {
