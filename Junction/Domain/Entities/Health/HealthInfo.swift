8e22d608db046256bad78ff275e72a8afbe8bea8
Eric <real.dassy@gmail.com>
2024-12-06 15:27:03 +0900

    [Feat] HealthInfo 모델 프로퍼티 네이밍 및 타입 변경
    기능 추가에 따른 모델 변경 사항입니다.

--- Junction/Domain/Entities/Health/HealthInfo.swift
+++ Junction/Domain/Entities/Health/HealthInfo.swift
@@ -11,24 +11,34 @@ import SwiftData
 @Model
 final class HealthInfo {
     var id: String
-    var pregnantWeek: PregnantWeek = PregnantWeek.early
-    var height: String = ""
-    var weight: String = ""
-    var bloodPressure: BloodPresure = BloodPresure.normal
-    var diabetes: Diabetes = Diabetes.notAffected
+    var gestationalWeek: GestationalWeek = GestationalWeek.early
+    var height: Double
+    var weight: Double
+    var lastHeightUnit: HeightUnit
+    var lastWeightUnit: WeightUnit
+    var bloodPressure: BloodPresure = BloodPresure.none
+    var diabetes: Diabetes = Diabetes.none
+    var restrictions: [Allergies] = []
+    var bmi: Double { weight / (height * height * 0.0001) }
     
-    var bmi: Double {
-        guard let weightValue = Double(weight), let heightValue = Double(height) else { return 0.0 }
-        return weightValue / heightValue * heightValue
-    }
-    
-    init(id: String = "HealthInfo", pregnantWeek: PregnantWeek, height: String, weight: String, bloodPressure: BloodPresure, diabetes: Diabetes) {
+    init(id: String = "HealthInfo",
+         gestationalWeek: GestationalWeek,
+         height: Double,
+         weight: Double,
+         lastHeightUnit: HeightUnit,
+         lastWeightUnit: WeightUnit,
+         bloodPressure: BloodPresure,
+         diabetes: Diabetes,
+         restrictions: [Allergies]) {
         self.id = id
-        self.pregnantWeek = pregnantWeek
+        self.gestationalWeek = gestationalWeek
         self.height = height
         self.weight = weight
+        self.lastHeightUnit = lastHeightUnit
+        self.lastWeightUnit = lastWeightUnit
         self.bloodPressure = bloodPressure
         self.diabetes = diabetes
+        self.restrictions = restrictions
     }
 }
 
@@ -37,11 +47,14 @@ extension HealthInfo: Convertible {
         let recordID = CKRecord.ID(recordName: self.id)
         let record = CKRecord(recordType: "HealthInfo", recordID: recordID)
         
-        record["pregnantWeek"] = self.pregnantWeek.rawValue as CKRecordValue
+        record["pregnantWeek"] = self.gestationalWeek.rawValue as CKRecordValue
         record["height"] = self.height as CKRecordValue
         record["weight"] = self.weight as CKRecordValue
+        record["lastHeightUnit"] = self.lastHeightUnit.rawValue
+        record["lastWeightUnit"] = self.lastWeightUnit.rawValue
         record["bloodPressure"] = self.bloodPressure.rawValue as CKRecordValue
         record["diabetes"] = self.diabetes.rawValue as CKRecordValue
+        record["restrictions"] = self.restrictions.map { $0.rawValue } as NSArray
         
         return record
     }
@@ -49,15 +62,30 @@ extension HealthInfo: Convertible {
     convenience init?(from record: CKRecord) {
         let id = record.recordID.recordName
         guard let pregnantWeekRaw = record["pregnantWeek"] as? String,
-              let pregnantWeek = PregnantWeek(rawValue: pregnantWeekRaw),
-              let height = record["height"] as? String,
-              let weight = record["weight"] as? String,
+              let pregnantWeek = GestationalWeek(rawValue: pregnantWeekRaw),
+              let height = record["height"] as? Double,
+              let weight = record["weight"] as? Double,
+              let heightUnitString = record["lastHeightUnit"] as? String,
+              let lastHeightUnit = HeightUnit(rawValue: heightUnitString),
+              let weightUnitString = record["lastWeightUnit"] as? String,
+              let lastWeightUnit = WeightUnit(rawValue: weightUnitString),
               let bloodPressureRaw = record["bloodPressure"] as? String,
               let bloodPressure = BloodPresure(rawValue: bloodPressureRaw),
               let diabetesRaw = record["diabetes"] as? String,
-              let diabetes = Diabetes(rawValue: diabetesRaw)
+              let diabetes = Diabetes(rawValue: diabetesRaw),
+              let restrictionsRaw = record["restrictions"] as? [String]
         else { return nil }
         
-        self.init(id: id, pregnantWeek: pregnantWeek, height: height, weight: weight, bloodPressure: bloodPressure, diabetes: diabetes)
+        let restrictions: [Allergies] = restrictionsRaw.compactMap { Allergies(rawValue: $0) }
+        
+        self.init(id: id,
+                  gestationalWeek: pregnantWeek,
+                  height: height,
+                  weight: weight,
+                  lastHeightUnit: lastHeightUnit,
+                  lastWeightUnit: lastWeightUnit,
+                  bloodPressure: bloodPressure,
+                  diabetes: diabetes,
+                  restrictions: restrictions)
     }
 }
