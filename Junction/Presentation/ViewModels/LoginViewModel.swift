f1ae79e61371dff52005cf50f4bc9ec8fd2af938
Eric <real.dassy@gmail.com>
2024-12-26 00:46:01 +0900

    [Refactor] UserSession 도입으로 인한 로직 이동
    ViewModel이 의존하고 있던 UseCase들이 UserSession으로 이동했습니다.

--- Junction/Presentation/ViewModels/LoginViewModel.swift
+++ Junction/Presentation/ViewModels/LoginViewModel.swift
@@ -8,12 +8,7 @@
 import AuthenticationServices
 import Foundation
 
-class LoginViewModel: ObservableObject {    
-    private let loginUseCase: LoginUseCase
-    
-    init(loginUseCase: LoginUseCase) {
-        self.loginUseCase = loginUseCase
-    }
+class LoginViewModel: ObservableObject {
     
     func prepareAppleLogin(request: ASAuthorizationAppleIDRequest) {
         let nonce = CryptoUtils.randomNonceString()
@@ -21,19 +16,15 @@ class LoginViewModel: ObservableObject {
         request.nonce = CryptoUtils.sha256(nonce)
     }
     
-    func handleAppleLoginCompletion(result: Result<ASAuthorization, Error>)  {
+    func makeAppleLoginCredential(result: Result<ASAuthorization, Error>, completion: @escaping (AppleAuthCredentialParameter) -> ())  {
         guard case .success(let authResult) = result, let appleIDCredential = authResult.credential as? ASAuthorizationAppleIDCredential else { return }
         guard let idToken = appleIDCredential.identityToken else { return }
         guard let idTokenString = String(data: idToken, encoding: .utf8) else { return }
         let fullName = appleIDCredential.fullName
     
-        Task {
-            do {
-                guard let nonce = CryptoUtils.getNonce() else { return }
-                let parameters = AppleAuthCredentialParameter(idToken: idTokenString, rawNonce: nonce, fullName: fullName)
-                try await loginUseCase.execute(parameter: parameters)
-            } catch { print(error) }
-        }
+        guard let nonce = CryptoUtils.getNonce() else { return }
+        let parameters = AppleAuthCredentialParameter(idToken: idTokenString, rawNonce: nonce, fullName: fullName)
+        completion(parameters)
     }
     
     
