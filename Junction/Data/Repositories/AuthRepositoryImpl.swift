742404af6d02116b414fcce9673ca8f4c625fdb1
Eric <real.dassy@gmail.com>
2024-12-26 00:38:36 +0900

    [Feat] AuthRepositoryImpl 클래스 구현

--- Junction/Data/Repositories/AuthRepositoryImpl.swift
+++ Junction/Data/Repositories/AuthRepositoryImpl.swift
@@ -15,9 +15,21 @@ class AuthRepositoryImpl: AuthRepository {
         self.authService = authService
     }
     
-    func logIn(parameters: AuthParameter) async throws {
-        guard let credential = AuthCredentialMapper.map(parameter: parameters) else { return }
-        try await authService.logIn(credential: credential)
+    func logIn(parameters: AuthParameter) async throws -> String {
+        guard let credential = AuthCredentialMapper.map(parameter: parameters) else { return "" }
+        let user = try await authService.logIn(credential: credential)
+        return user.uid
     }
     
+    func logout() throws { try authService.logOut() }
+    
+    func deleteAccount(userID: String) { authService.deleteAccount(userID: userID) }
+    
+    func observeAuthState(onChange: @escaping (String?) -> Void) -> AuthStateDidChangeListenerHandle {
+        return authService.observeAuthState(onChange: onChange)
+    }
+    
+    func removeAuthListener(_ handle: AuthStateDidChangeListenerHandle) {
+        authService.removeAuthListener(handle)
+    }
 }
