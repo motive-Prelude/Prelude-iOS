7e556b72c89ba783d6341b03cd63366e82404e71
Eric <real.dassy@gmail.com>
2024-12-26 00:43:26 +0900

    [Feat] UserSession 클래스 추가
    User의 상태 및 행동을 종합적으로 관리하는 session 입니당.

---
+++ Junction/Presentation/Sessions/UserSession.swift
@@ -0,0 +1,121 @@
+//
+//  UserStore.swift
+//  Junction
+//
+//  Created by 송지혁 on 11/10/24.
+//
+
+import Combine
+import FirebaseFirestore
+import Foundation
+
+
+class UserSession: ObservableObject {
+    @Published private(set) var userInfo: UserInfo?
+    private let userRepository: UserRepository
+    private let loginUseCase: LoginUseCase
+    private let logoutUseCase: LogOutUseCase
+    private let deleteAccountUseCase: DeleteAccountUseCase
+    private let observeAuthStateUseCase: ObserveAuthStateUseCase
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(userRepository: UserRepository,
+                 loginUseCase: LoginUseCase,
+                 logOutUseCase: LogOutUseCase,
+                 deleteAccountUseCase: DeleteAccountUseCase,
+                observeAuthStateUseCase: ObserveAuthStateUseCase) {
+        self.userRepository = userRepository
+        self.loginUseCase = loginUseCase
+        self.logoutUseCase = logOutUseCase
+        self.deleteAccountUseCase = deleteAccountUseCase
+        self.observeAuthStateUseCase = observeAuthStateUseCase
+        observeRepositoryChanges()
+        observeAuthState()
+    }
+    
+    func login(parameter: AuthParameter) async {
+        do {
+            let userID = try await loginUseCase.execute(parameter: parameter)
+            let userInfo = try await userRepository.fetch(userID: userID)
+            await MainActor.run { self.userInfo = userInfo }
+        } catch {
+            print(error)
+        }
+    }
+    
+    func logout() {
+        do {
+            try logoutUseCase.execute()
+            self.userInfo = nil
+        } catch {
+            
+        }
+    }
+    
+    func deleteAccount() {
+        guard let userID = userInfo?.id else { return }
+        deleteAccountUseCase.execute(userID: userID)
+        userRepository.delete(userID: userID)
+    }
+    
+    func syncCurrentUser() async throws {
+        guard let userID = userInfo?.id else { return }
+        do {
+            let newUserInfo = try await userRepository.fetch(userID: userID)
+            await MainActor.run { self.userInfo = newUserInfo }
+            
+        } catch { }
+    }
+    
+    @MainActor
+    func incrementSeeds(_ count: Int) async throws {
+        if count < 1 { assertionFailure("incrementSeeds 메서드에 음수가 들어갔어요.") }
+        
+        guard let userInfo = userInfo else { return }
+        do {
+            self.userInfo = try await userRepository.update(userID: userInfo.id, field: ["remainingTimes": FieldValue.increment(Int64(count))])
+        } catch { }
+    }
+    
+    @MainActor
+    func decrementSeeds(_ count: Int) async throws {
+        if count >= 0 { assertionFailure("incrementSeeds 메서드에 양수가 들어갔어요.") }
+        
+        guard let userInfo = userInfo else { return }
+        do {
+            self.userInfo = try await userRepository.update(userID: userInfo.id, field: ["remainingTimes": FieldValue.increment(Int64(-count))])
+        } catch {
+            
+        }
+    }
+    
+    private func observeRepositoryChanges() {
+        userRepository.iCloudChangeSubject
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                guard let self else { return }
+                guard let userInfo else { return }
+                Task {
+                    let newUserInfo = try await self.userRepository.syncFromICloud(userID: userInfo.id)
+                    await MainActor.run { self.userInfo = newUserInfo }
+                }
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func observeAuthState() {
+        observeAuthStateUseCase.startObserving { [weak self] userID in
+            guard let self = self else { return }
+            if let userID = userID {
+                Task {
+                    do {
+                        let userInfo = try await self.userRepository.fetch(userID: userID)
+                        await MainActor.run {
+                            self.userInfo = userInfo
+                        }
+                    } catch { print("UserInfo fetch 실패: \(error)") }
+                }
+            } else { self.userInfo = nil }
+        }
+    }
+}
