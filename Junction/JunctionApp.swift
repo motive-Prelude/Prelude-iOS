aab476174432c436cf018e0cc2979b74edaa6486
Eric <real.dassy@gmail.com>
2024-12-26 00:48:15 +0900

    [Feat] UserSession 클래스 Environment에 추가

--- Junction/JunctionApp.swift
+++ Junction/JunctionApp.swift
@@ -5,14 +5,24 @@
 //  Created by 송지혁 on 8/10/24.
 //
 
+import CloudKit
 import FirebaseCore
 import SwiftUI
 import SwiftData
 
 @main
 struct JunctionApp: App {
-    @StateObject var store = Store()
-    @StateObject var navigationManager = NavigationManager()
+    @StateObject private var diContainer = DIContainer.shared
+    @StateObject private var alertManager = AlertManager.shared
+    @StateObject private var store = Store()
+    @StateObject private var navigationManager = NavigationManager()
+    @StateObject private var userSession = UserSession(userRepository: DIContainer.shared.resolve(UserRepository.self)!,
+                                                       loginUseCase: DIContainer.shared.resolve(LoginUseCase.self)!,
+                                                       logOutUseCase: DIContainer.shared.resolve(LogOutUseCase.self)!,
+                                                       deleteAccountUseCase: DIContainer.shared.resolve(DeleteAccountUseCase.self)!,
+                                                       observeAuthStateUseCase: DIContainer.shared.resolve(ObserveAuthStateUseCase.self)!)
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     init() {
         FirebaseApp.configure()
@@ -21,12 +31,36 @@ struct JunctionApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .modelContainer(SwiftDataManager.shared.container)
-                .environmentObject(AlertManager.shared)
-                .environmentObject(DIContainer.shared)
+                .modelContainer(SwiftDataSource.shared.container)
+                .environmentObject(alertManager)
+                .environmentObject(diContainer)
                 .environmentObject(navigationManager)
                 .environmentObject(store)
+                .environmentObject(userSession)
+                
                 
         }
+        
     }
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        application.registerForRemoteNotifications()
+        return true
+    }
+    
+    func application(_ application: UIApplication,
+                     didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        let notification = CKNotification(fromRemoteNotificationDictionary: userInfo)
+
+        if notification?.subscriptionID == "UserInfoSubscriptionID" {
+            ICloudDataSource.shared.processNotification(userInfo: userInfo)
+        }
+        completionHandler(.newData)
+    }
+
+}
+    
