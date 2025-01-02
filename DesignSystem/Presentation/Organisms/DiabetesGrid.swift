5d0f35a56ea2901d25317e59ac3e849b6e973067
Eric <real.dassy@gmail.com>
2025-01-02 13:34:21 +0900

    [Feat] DiabetesGrid Organism 추가

---
+++ DesignSystem/Presentation/Organisms/DiabetesGrid.swift
@@ -0,0 +1,60 @@
+//
+//  DiabetesGrid.swift
+//  Junction
+//
+//  Created by 송지혁 on 12/30/24.
+//
+
+import SwiftUI
+
+struct DiabetesGrid: View {
+    let result: (Diabetes?) -> ()
+    @State private var diabetes: Diabetes?
+    
+    var body: some View {
+        Grid {
+            GridRow {
+                PLFormButton(label: Diabetes.none.rawValue,
+                             isSelected: bindingForDiabetes(Diabetes.none),
+                             contentType: .labelOnly,
+                             mode: .stretch)
+                .gridCellColumns(3)
+                .onTapGesture { diabetes = Diabetes.none }
+            }
+            
+            GridRow {
+                PLFormButton(label: Diabetes.type1.rawValue,
+                             isSelected: bindingForDiabetes(Diabetes.type1),
+                             contentType: .labelOnly,
+                             mode: .stretch)
+                .onTapGesture { diabetes = Diabetes.type1 }
+                
+                PLFormButton(label: Diabetes.type2.rawValue,
+                             isSelected: bindingForDiabetes(Diabetes.type2),
+                             contentType: .labelOnly,
+                             mode: .stretch)
+                .onTapGesture { diabetes = Diabetes.type2 }
+                
+                PLFormButton(label: Diabetes.gestational.rawValue,
+                             isSelected: bindingForDiabetes(Diabetes.gestational),
+                             contentType: .labelOnly,
+                             mode: .stretch)
+                .onTapGesture { diabetes = Diabetes.gestational }
+            }
+        }
+        .onChange(of: diabetes) { _, newValue in result(newValue) }
+    }
+    
+    private func bindingForDiabetes(_ diabetes: Diabetes) -> Binding<Bool> {
+        Binding(
+            get: { self.diabetes == diabetes },
+            set: { isSelected in
+                self.diabetes = isSelected ? diabetes : nil
+            }
+        )
+    }
+}
+
+#Preview {
+    DiabetesGrid() { _ in }
+}
