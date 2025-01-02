9955cba575850571f9ae0d4f67085978ccfd1fc2
Eric <real.dassy@gmail.com>
2025-01-02 13:34:33 +0900

    [Feat] FoodRestrictionGrid Organism 추가

---
+++ DesignSystem/Presentation/Organisms/FoodRestrictionGrid.swift
@@ -0,0 +1,37 @@
+//
+//  FoodRestrictionGrid.swift
+//  Junction
+//
+//  Created by 송지혁 on 12/30/24.
+//
+
+import SwiftUI
+
+struct FoodRestrictionGrid: View {
+    let result: ([Allergies]) -> ()
+    @State private var allergies: [Allergies] = []
+    
+    var body: some View {
+        FlowLayout(spacing: 8) {
+            ForEach(Allergies.allCases, id: \.self) { allergy in
+                PLFormButton(label: allergy.rawValue,
+                             isSelected: bindingForAllergy(allergy),
+                             contentType: .labelOnly,
+                             mode: .hug)
+                
+            }
+        }
+        .onChange(of: allergies) { _, newValue in result(allergies) }
+    }
+    
+    private func bindingForAllergy(_ allergy: Allergies) -> Binding<Bool> {
+        return Binding(
+            get: { allergies.contains(allergy) },
+            set: { isSelected in isSelected ? allergies.append(allergy) : allergies.removeAll { $0 == allergy } }
+        )
+    }
+}
+
+#Preview {
+    FoodRestrictionGrid() { _ in }
+}
