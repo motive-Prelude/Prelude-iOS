406787b82948975bf90286ec8ebbc01a1427c1ee
Eric <real.dassy@gmail.com>
2025-02-18 08:40:16 +0900

    [Feat] applyConditionalSafeArea View Extension 추가

--- DesignSystem/Extensions/View+.swift
+++ DesignSystem/Extensions/View+.swift
@@ -67,3 +67,16 @@ extension View {
         self.modifier(ScreenTrackingModifier(screenName: screenName))
     }
 }
+
+struct ConditionalSafeAreaModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        let safeEdges: Edge.Set = UIDevice.current.userInterfaceIdiom == .pad ? [.horizontal] : [.horizontal, .top, .bottom]
+        return content.ignoresSafeArea(.all, edges: safeEdges)
+    }
+}
+
+extension View {
+    func applyConditionalSafeArea() -> some View {
+        self.modifier(ConditionalSafeAreaModifier())
+    }
+}
