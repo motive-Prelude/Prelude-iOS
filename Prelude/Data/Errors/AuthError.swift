a4e432c8284b782aa2bfb56d38dab62bd40f0693
Eric <real.dassy@gmail.com>
2025-03-27 23:24:50 +0900

    [Delete] AuthError에서 FirebaseAuth 의존성 제거

--- Prelude/Data/Errors/AuthError.swift
+++ Prelude/Data/Errors/AuthError.swift
@@ -5,8 +5,6 @@
 //  Created by 송지혁 on 1/2/25.
 //
 
-import FirebaseAuth
-
 enum AuthError: Error {
     case networkError
     case sessionExpired
@@ -16,20 +14,4 @@ enum AuthError: Error {
     case userMismatch
     case tooManyRequests
     case unknown
-    
-    init(from error: AuthErrorCode) {
-        switch error {
-            case .accountExistsWithDifferentCredential: self = .invalidCredential
-            case .credentialAlreadyInUse: self = .invalidCredential
-            case .invalidCredential: self = .invalidCredential
-            case .networkError: self = .networkError
-            case .tooManyRequests: self = .tooManyRequests
-            case .sessionExpired: self = .sessionExpired
-            case .userDisabled: self = .userDisabled
-            case .userNotFound: self = .userNotFound
-            case .userMismatch: self = .userMismatch
-            default: self = .unknown
-            
-        }
-    }
 }
