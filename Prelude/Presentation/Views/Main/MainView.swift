e0c8003efbd7447495e4946a2014abfb92f29009
Eric <real.dassy@gmail.com>
2025-04-11 02:23:13 +0900

    [Delete] MainView Preview 제거

--- Prelude/Presentation/Views/Main/MainView.swift
+++ Prelude/Presentation/Views/Main/MainView.swift
@@ -212,13 +212,3 @@ struct MainView: View {
         }
     }
 }
-
-#Preview {
-    MainView()
-        .environmentObject( UserSession(userRepository: DIContainer.shared.resolve(UserRepository.self)!,
-                                        loginUseCase: DIContainer.shared.resolve(LoginUseCase.self)!,
-                                        logOutUseCase: DIContainer.shared.resolve(LogOutUseCase.self)!, reauthenticateUseCase: DIContainer.shared.resolve(ReauthenticateUseCase.self)!,
-                                        deleteAccountUseCase: DIContainer.shared.resolve(DeleteAccountUseCase.self)!,
-                                        observeAuthStateUseCase: DIContainer.shared.resolve(ObserveAuthStateUseCase.self)!))
-        .environmentObject(NavigationManager())
-}
