69aab5a1c9fa9ca7e1af9c20889624051b02bb8e
Eric <real.dassy@gmail.com>
2025-04-11 01:40:10 +0900

    [Refactor] LoginUseCase에서 UserSyncService를 의존하도록 변경

--- Prelude/Domain/UseCases/Auth/LoginUseCase.swift
+++ Prelude/Domain/UseCases/Auth/LoginUseCase.swift
@@ -9,32 +9,32 @@ import Foundation
 
 class LoginUseCase {
     private let authRepository: AuthRepository
-    private let userRepository: UserRepository
+    private let userSyncService: UserSyncService
     
-    init(authRepository: AuthRepository, userRepository: UserRepository) {
+    init(authRepository: AuthRepository, userSyncService: UserSyncService) {
         self.authRepository = authRepository
-        self.userRepository = userRepository
+        self.userSyncService = userSyncService
     }
     
     
     func execute(parameter: AuthParameter) async throws(DomainError) -> UserInfo {
         do {
             let (userID, sub) = try await authRepository.logIn(parameter: parameter)
-            let deletedUser = await userRepository.checkRejoinUser(from: sub)
+            let deletedUser = try await userSyncService.checkRejoinUser(id: sub)
             
             if let deletedUser {
                 let rejoinUser = UserInfo(id: userID, remainingTimes: 0, didReceiveGift: deletedUser.didReceiveGift)
-                try await userRepository.create(collection: "User", user: rejoinUser)
-                try await userRepository.delete(collection: "Deleted User", userID: sub)
+                try await userSyncService.save(user: rejoinUser, in: .active)
+                try await userSyncService.delete(id: sub, from: .deleted)
                 
                 return rejoinUser
             } else {
-                let userInfo = try await userRepository.fetch(collection: "User", userID: userID)
+                let userInfo = try await userSyncService.fetch(id: userID, from: .active)
                 
                 return userInfo
             }
         }
-        catch let error as AuthError { throw parseError(error) }
+        catch let error as RepositoryError { throw ErrorMapper.mapToDomain(error) }
         catch let error as DomainError { throw error }
         catch { throw .unknown }
     }
