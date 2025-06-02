09c352663c4d70700f631c3f3c3fae4dead5ea9d
Eric <real.dassy@gmail.com>
2025-06-02 17:13:56 +0900

    [Feat] ButtonType destructive case 추가

--- DesignSystem/Style/Button/ButtonType.swift
+++ DesignSystem/Style/Button/ButtonType.swift
@@ -1,13 +1,14 @@
 //
-//  Type.swift
-//  Junction
+//  ButtonType.swift
+//  Prelude
 //
-//  Created by 송지혁 on 11/28/24.
+//  Created by 송지혁 on 5/31/25.
 //
 
 enum ButtonType: Hashable {
     case primary
     case secondary
+    case destructive
     case form
 }
 
