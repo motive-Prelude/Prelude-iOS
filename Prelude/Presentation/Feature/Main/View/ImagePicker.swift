cfb3c14ee621c5a0c67bf744b3dd8fe069477ef0
Eric <real.dassy@gmail.com>
2025-06-02 18:48:39 +0900

    [Refactor] ImagePicker의 UIImage release 방식 변경(Binding -> Closure)

--- Prelude/Presentation/Feature/Main/View/ImagePicker.swift
+++ Prelude/Presentation/Feature/Main/View/ImagePicker.swift
@@ -10,8 +10,8 @@ import UIKit
 
 struct ImagePicker: UIViewControllerRepresentable {
     @Environment(\.presentationMode) var presentationMode
-    @Binding var image: UIImage?
     var sourceType: UIImagePickerController.SourceType = .camera
+    var imagePicked: (UIImage) -> ()
     
     class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
         let parent: ImagePicker
@@ -22,7 +22,7 @@ struct ImagePicker: UIViewControllerRepresentable {
         
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             if let uiImage = info[.originalImage] as? UIImage {
-                parent.image = uiImage
+                parent.imagePicked(uiImage)
             }
             parent.presentationMode.wrappedValue.dismiss()
         }
